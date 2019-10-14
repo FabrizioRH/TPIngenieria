d4d2916b16227b827b02d5e30a7ba551f9c5350e
Fabrizio Helmbold <frhelmbold@gmail.com>
2019-10-14 12:29:42 -0300

    Modificados algunos tests y realizados los faltantes

--- tests/FuncionesBasicasTest.php
+++ tests/FuncionesBasicasTest.php
@@ -89,5 +89,39 @@ class FuncionesBasicasTest extends \PHPUnit\Framework\TestCase {
 
 //Pruebas para EmpleadoPermanente
 
+public function testGetFechaIngresoParaUnaFechaDeIngresoIngresadaManualmente() {
+	$i = $this->crear("Nombre4", "Apellido4", "11111111", "35000", "2019-10-10");
+	$this->assertEquals("2019-10-10", $i->getFechaIngreso());
+}
+
+public function testCalcularComisionConResultadoEsperadoRespectoAlDia141019() {
+	$i = $this->crear("Nombre4", "Apellido4", "11111111", "35000", "2019-10-10");
+	$this->assertEquals(4, $i->calcularComision());
+}
+
+public function testCalcularIngresoTotalConResultadoEsperadoRespectoAlDia141019() {
+	$i = $this->crear("Nombre4", "Apellido4", "11111111", "35000", "2019-10-10");
+	$this->assertEquals(36400, $i->calcularIngresoTotal());
+}
+
+public function testCalcularantiguedadConResultadoEsperadoRespectoAlDia141019() {
+	$i = $this->crear("Nombre4", "Apellido4", "11111111", "35000", "2012-10-10");
+	$this->assertEquals(2560, $i->calcularAntiguedad());
+}
+
+public function testGetFechaIngresoParaUnaFechaDeIngresoSinEspecificarConRespectoAlDia141019() {
+	$i = $this->crear("Nombre4", "Apellido4", "11111111", "35000");
+	$this->assertEquals("2019-10-14", $i->getFechaIngreso());
+}
+
+public function testCalcularAntiguedadParaUnaFechaDeIngresoSinEspecificarConRespectoAlDia141019() {
+	$i = $this->crear("Nombre4", "Apellido4", "11111111", "35000");
+	$this->assertEquals(0, $i->getFechaIngreso());
+}
+
+public function testNoSePuedeCrearEmpleadoConFechaDeIngresoPosteriorALaActualConRespectoAlDia141019() {
+	$this->expectException(\Exception::class);
+	$i = $this->crear("Nombre4", "Apellido4", "11111111", "35000", "2019-10-20");
+}
 
 }
